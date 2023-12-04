412578766455bdffd88aec634ea726df64b34a25
Eduardo Barbosa <edo@mothervolcano.com>
2023-12-04 18:30:13 +0000

    style sliders, add size prop to component

--- src/components/consoles/default.tsx
+++ src/components/consoles/default.tsx
@@ -1,5 +1,10 @@
-import { Text, Slider, Stack } from "@mantine/core";
+import { Text, Slider, Stack, DEFAULT_THEME, rem } from "@mantine/core";
 import sliderStyles from "../../styles/slider.module.css";
+
+const dark = DEFAULT_THEME.colors.dark[5];
+const softDark = DEFAULT_THEME.colors.dark[0];
+const light = DEFAULT_THEME.colors.gray[0];
+const softLight = DEFAULT_THEME.colors.gray[2];
 
 interface ConsoleProps {
 	params: any;
@@ -9,18 +14,18 @@ interface ConsoleProps {
 }
 
 interface Style {
-    width: string;
-    display: string;
-    flexDirection: React.CSSProperties['flexDirection'];
-    justifyContent?: React.CSSProperties['justifyContent'];
-    gap?: string;
-
+	width: string;
+	display: string;
+	flexDirection: React.CSSProperties["flexDirection"];
+	justifyContent?: React.CSSProperties["justifyContent"];
+	gap?: string;
 }
 
 const labelStyleProps = {
 	size: "xs",
 	fw: "500",
 	c: "var(--mantine-color-dark-3)",
+	pb: "0.30rem",
 };
 
 const StyledText = (props: any) => {
@@ -51,56 +56,90 @@ const DefaultConsole = (props: ConsoleProps) => {
 		display: "flex",
 		flexDirection: "row",
 		justifyContent: "space-between",
-		gap: "1rem"
-	}
+		gap: "1rem",
+	};
 
 	const colLayout: Style = {
 		width: "100%",
 		display: "flex",
 		flexDirection: "column",
-	}
+	};
+
+	const gap = size === "COMPACT" ? "0.75rem" : "1rem";
 
 	return (
 		<div style={layout === "ROW" ? rowLayout : colLayout}>
-			<div style={{width: "100%"}}>
-				{params.filter((p:any) => p.rank === 1).map((p: any) => (
-					<Stack key={p.id} gap={2}>
-						<StyledText>{p.label}</StyledText>
-						<Slider
-							id={p.id}
-							name={p.id}
-							min={p.range[0]}
-							max={p.range[1]}
-							step={p.step}
-							onChange={(value) => {
-								handleSliderInput(value, p.id);
+			<div style={{ width: "100%" }}>
+				{params
+					.filter((p: any) => p.rank === 1)
+					.map((p: any) => (
+						<div
+							key={p.id}
+							style={{
+								display: "flex",
+								flexDirection: "column",
+								justifyContent: "space-between",
 							}}
-							value={p.value}
-							classNames={sliderStyles}
-						/>
-					</Stack>
-				))}
+						>
+							<div style={{ width: "100%", paddingBottom: gap }}>
+								<StyledText>{p.label}</StyledText>
+								<Slider
+									id={p.id}
+									name={p.id}
+									min={p.range[0]}
+									max={p.range[1]}
+									step={p.step}
+									onChange={(value) => {
+										handleSliderInput(value, p.id);
+									}}
+									value={p.value}
+									size="1px"
+									thumbSize={rem(10)}
+									color={dark}
+									showLabelOnHover={false}
+									styles={{ thumb: { backgroundColor: dark, borderWidth: 0 } }}
+									classNames={sliderStyles}
+								/>
+							</div>
+						</div>
+					))}
 			</div>
-			<div style={{width: "100%"}}>
-				{params.filter((p:any) => p.rank === 2).map((p: any) => (
-					<Stack key={p.id} gap={2}>
-						<StyledText>{p.label}</StyledText>
-						<Slider
-							id={p.id}
-							name={p.id}
-							min={p.range[0]}
-							max={p.range[1]}
-							step={p.step}
-							onChange={(value) => {
-								handleSliderInput(value, p.id);
+			<div style={{ width: "100%" }}>
+				{params
+					.filter((p: any) => p.rank === 2)
+					.map((p: any) => (
+						<div
+							key={p.id}
+							style={{
+								display: "flex",
+								flexDirection: "column",
+								justifyContent: "space-between",
 							}}
-							value={p.value}
-							classNames={sliderStyles}
-						/>
-					</Stack>
-				))}
+						>
+							<div style={{ width: "100%", paddingBottom: gap }}>
+								<StyledText>{p.label}</StyledText>
+								<Slider
+									id={p.id}
+									name={p.id}
+									min={p.range[0]}
+									max={p.range[1]}
+									step={p.step}
+									onChange={(value) => {
+										handleSliderInput(value, p.id);
+									}}
+									value={p.value}
+									size="1px"
+									thumbSize={rem(10)}
+									color={dark}
+									showLabelOnHover={false}
+									styles={{ thumb: { backgroundColor: dark, borderWidth: 0 } }}
+									classNames={sliderStyles}
+								/>
+							</div>
+						</div>
+					))}
 			</div>
-	    </div>
+		</div>
 	);
 };
 
