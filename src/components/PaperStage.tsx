b524e234b9e9e9b4e567e0579c030abe57fd8081
Eduardo Barbosa <edo@mothervolcano.com>
2023-12-13 23:43:09 +0000

    prepare compontent for deprecation

--- src/components/PaperStage.tsx
+++ src/components/PaperStage.tsx
@@ -1,14 +1,17 @@
-import { useRef, useEffect, useLayoutEffect, useState } from "react";
+import { useRef, useEffect, useLayoutEffect, useState, RefObject } from "react";
 
 import { PaperScope } from "paper";
 import useResizeObserver from "@react-hook/resize-observer";
 
 export const paperScope = new PaperScope();
 
-const useSize = (target: any): any => {
-	const [size, setSize] = useState();
+const useSize = (target: RefObject<HTMLElement>): DOMRect | undefined => {
+	const [size, setSize] = useState<DOMRect>();
 
 	useLayoutEffect(() => {
+		if (!target || !target.current) {
+			throw new Error(`ERROR @useSize hook: element not found`);
+		}
 		setSize(target.current.getBoundingClientRect());
 	}, [target]);
 
@@ -32,22 +35,21 @@ const PaperStage = ({ onPaperLoad, onResize }: any) => {
 		}
 	}, []);
 
-
 	useEffect(() => {
-	    // Update canvas size based on parent size
-	    if (canvasRef.current && size) {
-	        canvasRef.current.width = size.width;
-	        canvasRef.current.height = size.height;
-	        onResize({width: size.width, height: size.height});
-	    }
+		// Update canvas size based on parent size
+		if (canvasRef.current && size) {
+			canvasRef.current.width = size.width;
+			canvasRef.current.height = size.height;
+			onResize({ width: size.width, height: size.height });
+		}
 	}, [size]);
 
 	const canvasWidth = size ? `${size.width}px` : "100%";
 	const canvasHeight = size ? `${size.height}px` : "100%";
 
 	return (
-	    <div ref={containerRef} style={{width: "100%", height: "100%"}}>
-			<canvas style={{position: "relative", width: canvasWidth, height: canvasHeight}} ref={canvasRef}></canvas>
+		<div ref={containerRef} style={{ width: "100%", height: "100%" }}>
+			<canvas style={{ position: "relative", width: canvasWidth, height: canvasHeight }} ref={canvasRef}></canvas>
 		</div>
 	);
 };
